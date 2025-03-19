f05b108ec7924c260c69b88a456ef1b8403ad28c
Proyectos X57 <proyectos.d57x@gmail.com>
2025-03-19 18:17:40 -0500

    update : colores

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@
 
 
     // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
+    $(".btn-contactame").on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
             
@@ -26,6 +26,51 @@
             }
         }
     });
+
+    //
+    $(".navbar-nav a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+        // alert("gola")
+        $('.navbar-collapse').removeClass('show');
+
+    });
+
+    //
+    $(".btn-ir-proyectos").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 45
+            }, 1500, 'easeInOutExpo');
+            
+            if ($(this).parents('.navbar-nav').length) {
+                $('.navbar-nav .active').removeClass('active');
+                $(this).closest('a').addClass('active');
+            }
+        }
+    });
+
+    //Cerrar menu
+    document.addEventListener("click", (e) => {
+        if (!document.getElementById("menu-principal").contains(e.target)) {
+        //   menu.style.display = "none";
+        // alert("aa")
+        $('.navbar-collapse').removeClass('show');
+
+        }
+      });
 
 
     // Typed Initiate
